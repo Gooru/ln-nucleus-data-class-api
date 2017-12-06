f297e2a311e45fa943c5cec03f117f409d5f0cc3
gooru <mukul@gooru.org>
2017-12-07 01:42:43 +0530

    DCA: Student Assessment Performance Report for Teachers

--- src/main/java/org/gooru/nucleus/dataclass/api/gateway/routes/DataClassApiReadConfigurator.java
+++ src/main/java/org/gooru/nucleus/dataclass/api/gateway/routes/DataClassApiReadConfigurator.java
@@ -519,6 +519,17 @@ class DataClassApiReadConfigurator implements RouteConfigurator {
               options, reply -> new RouteResponseUtility().responseHandler(routingContext, reply, LOG));
       });
       
+      //DCA Assessment Performance (Teacher)
+      router.get(RouteConstants.DCA_ASSESSMENT_STUDENTS_PERF_GET).handler(routingContext -> {            
+          String classId = routingContext.request().getParam(RouteConstants.ID_CLASS);
+          String assessmentId = routingContext.request().getParam(RouteConstants.ID_COLLECTION);
+          DeliveryOptions options = new DeliveryOptions().setSendTimeout(mbusTimeout * 1000)
+              .addHeader(MessageConstants.MSG_HEADER_OP, MessageConstants.MSG_OP_DCA_STUDENT_ASSESSMENT_PERF)
+              .addHeader(RouteConstants.ID_CLASS, classId).addHeader(RouteConstants.ID_COLLECTION, assessmentId);
+          eb.send(MessagebusEndpoints.MBEP_DATACLASS_API, new RouteRequestUtility().getBodyForMessage(routingContext),
+              options, reply -> new RouteResponseUtility().responseHandler(routingContext, reply, LOG));
+      });
+      
       //*************** RUBRICS GRADING********************************************************************************
       
       //Get Questions pending grading
